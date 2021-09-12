47e2e4bf1cf53ebfb53f47bdf6a4a77c8925abed
Stephane Mensah <waptik@users.noreply.github.com>
2021-09-12 09:19:34 +0000

    notify admin after webhook is set

--- src/app/server/env.ts
+++ src/app/server/env.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, str } from "envalid";
+import { cleanEnv, str, num } from "envalid";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,4 +6,9 @@ dotenv.config();
 export default cleanEnv(process.env, {
   BOT_TOKEN: str({ desc: "The Telegram bot API token" }),
   DETA_PATH: str({ default: undefined }),
+  ADMIN: num({ default: undefined }),
+  PORT: num({
+    desc: "Server port",
+    default: (process.env.PORT as unknown as number) || 8080,
+  }),
 });
--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -1,12 +1,9 @@
 import { run } from "@grammyjs/runner";
-import dotenv from "dotenv";
 import { webhookCallback } from "grammy";
 
 import app from "./express";
 import bot from "./bot";
 import env from "./env";
-
-dotenv.config();
 
 function main() {
   if (!env.DETA_PATH) {
@@ -16,18 +13,25 @@ function main() {
     return;
   }
 
-  const port = process.env.PORT || 8080;
   app.use(webhookCallback(bot));
-  app.listen(port, async () => {
+  app.listen(env.PORT, async () => {
     const webhookUrl = `https://${env.DETA_PATH}.deta.dev`;
 
-    console.log("Server listening on port", process.env.PORT);
+    console.log("Server listening on port", env.PORT);
 
     await bot.api.deleteWebhook();
     console.log(`Webhook deleted`);
     await bot.api.setWebhook(webhookUrl, {
       drop_pending_updates: true,
     });
+
+    // notify admin about deployment if process.env.ADMIN is set
+    if (env.ADMIN) {
+      await bot.api.sendMessage(
+        env.ADMIN,
+        `Bot connected to webhookUrl ${webhookUrl}`
+      );
+    }
   });
 }
 
